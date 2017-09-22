591ccae6687b853ca88dcda3ac51c8bba53e979d
That guy <daniel.barnett@usm.edu>
2017-09-21 19:29:45 -0500

    Here's some spam for that sandwich!

---
+++ simpleCode.cpp
@@ -0,0 +1,16 @@
+include iostream;
+
+using namespace std;
+
+int main()
+{
+	// Just a little something to annoy and confuse the unwary
+	for (int i = 0; i < 100; i++)
+	{
+		cout << "Release the Spam! Count: " << i << "!" << endl;
+	}
+
+	cout << "You just got spammed in C++!" << endl;
+
+	return;
+}
